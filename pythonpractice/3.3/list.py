0d0ee419c78f1e603f03ce7a4ef332459fd5385a
cahn <359692713@qq.com>
2018-01-10 21:21:03 +0800

    updata

---
+++ pythonpractice/3.3/list.py
@@ -0,0 +1,22 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(sorted(cars,reverse=True))
+print(cars)
+cars.reverse()
+print(cars)
+print(len(cars))
+
+places = ['shenzhen', 'chengdu', 'tokyo', 'seattle', 'new york']
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places,reverse=True))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+print(len(places))
